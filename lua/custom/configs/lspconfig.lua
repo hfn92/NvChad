1ca394ecdad79e619cd3ff80ba49ecd083744502
fab <FH2506@gmail.com>
2023-05-17 21:16:32 +0200

    glsl

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,7 +1,20 @@
 --local on_attach = require("plugins.configs.lspconfig").on_attach
 --local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-require'lspconfig'.clangd.setup{ on_attach = require("lsp-format").on_attach }
---require'lspconfig'.clangformat.setup{}
+local setup = {
+  on_attach = function(client, bufnr)
+    require "lsp_signature".on_attach(client, bufnr)  -- Note: add in lsp client on-attach
+    require("lsp-format").on_attach (client, bufnr)
+  end
+}
 
+local lspconfig = require "lspconfig"
+--require'lspconfig'.clangd.setup{ on_attach = require("lsp-format").on_attach }
+require'lspconfig'.clangd.setup{
+  on_attach = function(client, bufnr)
+    require "lsp_signature".on_attach(client, bufnr)  -- Note: add in lsp client on-attach
+    require("lsp-format").on_attach (client, bufnr)
+  end
+}
+require'lspconfig'.cmake.setup{}
+require'lspconfig'.glslls.setup{ { filetypes = "fsh" } }
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,5 @@
 vim.opt.colorcolumn = "120"
 vim.opt.relativenumber = true
 
+vim.filetype.add({ extension = { fsh = 'glsl' } })
+vim.filetype.add({ extension = { vsh = 'glsl' } })
