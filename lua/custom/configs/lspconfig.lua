25d85e12e696cf59e51d73e2cfb2355450572ed4
fab <FH2506@gmail.com>
2023-05-18 17:27:38 +0200

    stuff

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -9,12 +9,16 @@ local setup = {
 }
 
 local lspconfig = require "lspconfig"
+    local ccapabilities = vim.lsp.protocol.make_client_capabilities()
+    ccapabilities.offsetEncoding = { "utf-16" }
 --require'lspconfig'.clangd.setup{ on_attach = require("lsp-format").on_attach }
 require'lspconfig'.clangd.setup{
   on_attach = function(client, bufnr)
     require "lsp_signature".on_attach(client, bufnr)  -- Note: add in lsp client on-attach
     require("lsp-format").on_attach (client, bufnr)
-  end
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+  capabilities = ccapabilities
 }
 require'lspconfig'.cmake.setup{}
 require'lspconfig'.glslls.setup{ { filetypes = "fsh" } }
