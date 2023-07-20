7a1d964478daf1a167b6d7e3b9cca28d4a72830b
fab <FH2506@gmail.com>
2023-07-20 07:52:33 +0200

    coverage

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -65,6 +65,23 @@ local plugins = {
     cmd = "PerfLoadCallGraph",
   },
   {
+    "andythigpen/nvim-coverage",
+    cmd = { "CoverageLoadLcov" },
+    config = function()
+      require("coverage").setup {
+        signs = {
+          covered = { hl = "CoverageCovered", text = "▎" },
+          uncovered = { hl = "CoverageUncovered", text = "X" },
+          partial = { hl = "CoverageCovered", text = "▎" },
+        },
+
+        load_coverage_cb = function(ftype)
+          vim.notify("Loaded " .. ftype .. " coverage")
+        end,
+      }
+    end,
+  },
+  {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
