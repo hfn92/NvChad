1306a39eb0c68b1460a01ae1cef300a1289e7430
hfn92 <Fab.Hoersch@gmail.com>
2024-02-29 07:44:04 +0100

    neogen

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -287,9 +287,9 @@ local plugins = {
     --  dir = '/home/fab/Desktop/cmake-gtest.nvim/',
     --   -- dev=true,
     --   lazy=false,
-    --   config = function()
-    --      require("cmake-gtest").setup{}
-    --    end,
+    config = function()
+      require("cmake-gtest").setup {}
+    end,
   },
   {
     -- "Civitasv/cmake-tools.nvim",
@@ -494,13 +494,34 @@ local plugins = {
     cmd = { "TSPlaygroundToggle" },
   },
   {
-    "hfn92/neogen",
-    branch = "cpp_alt_comment_style",
+    "danymat/neogen",
+    -- branch = "cpp_alt_comment_style",
     dependencies = "nvim-treesitter/nvim-treesitter",
     cmd = { "Neogen" },
     config = function()
+      local i = require("neogen.types.template").item
       require("neogen").setup {
         snippet_engine = "luasnip",
+
+        languages = {
+          cpp = {
+            template = {
+              annotation_convention = "my_doxygen",
+              my_doxygen = {
+                { nil, "/// \\file", { no_results = true, type = { "file" } } },
+                { nil, "/// \\brief $1", { no_results = true, type = { "func", "file", "class" } } },
+                { nil, "", { no_results = true, type = { "file" } } },
+
+                { i.ClassName, "/// \\class %s", { type = { "class" } } },
+                { i.Type, "/// \\typedef %s", { type = { "type" } } },
+                { nil, "/// \\brief $1", { type = { "func", "class", "type" } } },
+                { i.Tparam, "/// \\tparam %s $1" },
+                { i.Parameter, "/// \\param %s $1" },
+                { i.Return, "/// \\return $1" },
+              },
+            },
+          },
+        },
 
         placeholders_text = {
           ["description"] = "",
