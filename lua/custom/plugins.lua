69c620a7d4eb71c2bde038a4eff149c81895372f
hfn92 <Fab.Hoersch@gmail.com>
2024-03-11 20:10:13 +0100

    fix

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -384,8 +384,8 @@ local plugins = {
           enable = true,
           prev_selection = ",", -- (Optional) keymap to select the previous selection
           keymaps = {
-            ["os"] = "string",
-            ["s"] = "string_inner",
+            ["oa"] = "string",
+            ["a"] = "string_inner",
             ["."] = "textsubjects-smart",
             [";"] = "textsubjects-container-outer",
             ["i;"] = "textsubjects-container-inner",
@@ -411,8 +411,8 @@ local plugins = {
               ["aa"] = "@parameter.outer",
               ["ia"] = "@parameter.inner",
               -- ["at"] = "@type.inner",
-              -- ["af"] = "@function.outer",
-              -- ["if"] = "@function.inner",
+              ["F"] = "@function.outer",
+              ["f"] = "@function.inner",
               -- ["ac"] = "@class.outer",
               -- -- You can optionally set descriptions to the mappings (used in the desc parameter of
               -- -- nvim_buf_set_keymap) which plugins like which-key display
@@ -442,6 +442,44 @@ local plugins = {
             -- * selection_mode: eg 'v'
             -- and should return true of false
             include_surrounding_whitespace = true,
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]]"] = { query = "@class.outer", desc = "Next class start" },
+              --
+              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+              -- ["]o"] = "@loop.*",
+              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+              --
+              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+              -- ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+              -- ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]["] = "@class.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[["] = "@class.outer",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[]"] = "@class.outer",
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {
+              ["]d"] = "@conditional.outer",
+            },
+            goto_previous = {
+              ["[d"] = "@conditional.outer",
+            },
           },
         },
       }
